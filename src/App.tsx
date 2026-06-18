d1e9268e7019bc2e79cc2d8d71075381adf91faa
wheatgreaser3000 <wheatgreaser3000@gmail.com>
2026-06-19 00:22:59 +0530

    feat: one more erro

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
   var [showQuery, setShowQuery] = useState(true);
   var [showResults, setShowResults] = useState(false);
   var [jsonval, setJsonval] = useState({"name":" ", "desc":" ", "score":" "});
-  function handleChange(e: React.FormEvent<HTMLInputElement>){
+  function handleChange(e: React.KeyboardEvent<HTMLInputElement>){
     setInputVal((e.target as HTMLInputElement).value);
   }
-  function changePage(e: React.FormEvent<HTMLInputElement>){
+  function changePage(e: React.KeyboardEvent<HTMLInputElement>){
   
   if(e.key == 'Enter'){
     setInputVal((e.target as HTMLInputElement).value);
